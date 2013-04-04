3e181f83f53c38d2a2b2a5c39257b2c2dacee3b4
Michael Bianco <iloveitaly@gmail.com>
2013-04-04 11:10:15 -0400

    Adding titles to the author pages

--- app/controllers/spree/authors_controller.rb
+++ app/controllers/spree/authors_controller.rb
@@ -4,10 +4,11 @@ module Spree
   
   class AuthorsController < BaseController
     before_filter :load_author, :only => :show
-    # rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+    rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
     def index
       @authors = Spree::Author.all
+      @title = t(:author_index_title)
     end
 
     def show; end
@@ -15,6 +16,7 @@ module Spree
     private
       def load_author
         @author = Author.find_by_permalink!(params[:id])
+        @title = t(:author_title, :author => @author.name)
       end
   end
 end
